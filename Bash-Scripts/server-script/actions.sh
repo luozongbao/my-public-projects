149b300c349944fb7141501221c414f9ad57f9c7
Atipat Lorwongam <atipat.lorwongam@outlook.com>
2021-01-29 12:24:47 +0700

    add automation to mysql_secure_installation

--- Bash-Scripts/server-script/actions.sh
+++ Bash-Scripts/server-script/actions.sh
@@ -847,26 +847,15 @@ function InstallNetDATA
 
 function securemysql
 {
-    #mysql_secure_installation 2>>$ERRORFILE
-    SECURE_MYSQL=$(expect -c "
-    set timeout 10
-    spawn mysql_secure_installation
-    expect \"Enter current password for root (enter for none):\"
-    send \"\r\"
-    expect \"Change the root password?\"
-    send \"n\r\"
-    expect \"Remove anonymous users?\"
-    send \"y\r\"
-    expect \"Disallow root login remotely?\"
-    send \"y\r\"
-    expect \"Remove test database and access to it?\"
-    send \"y\r\"
-    expect \"Reload privilege tables now?\"
-    send \"y\r\"
-    expect eof
-    ")
+mysql_secure_installation <<EOF
 
-    echo "$SECURE_MYSQL"
+n
+y
+y
+y
+y
+y
+EOF
 }
 
 
